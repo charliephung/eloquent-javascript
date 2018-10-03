4562d4fcf74ed8206d6e8a0258c41c052f7fcac9
charlie <charliephung@gmail.com>
2018-10-03 14:35:19 +0300

    ptc

---
+++ functional/6-recursion-ptc.js
@@ -0,0 +1,10 @@
+// must in strict mode
+"use strict";
+
+const sum = (sum, num, ...rest) => {
+  sum += num;
+  if (rest.length == 0) return sum;
+  // Now stack frames dont have to keep track of current sum
+  // since we pass it along with the function call
+  return sum(sum, ...rest);
+};
