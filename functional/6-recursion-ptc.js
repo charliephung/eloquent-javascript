5721040fe6fb0b95aabc50dfd5c3da5f0a0b1c3a
charlie <charliephung@gmail.com>
2018-10-03 14:37:28 +0300

    ptc

--- functional/6-recursion-ptc.js
+++ functional/6-recursion-ptc.js
@@ -6,5 +6,6 @@ const sum = (sum, num, ...rest) => {
   if (rest.length == 0) return sum;
   // Now stack frames dont have to keep track of current sum
   // since we pass it along with the function call
+  //   before is sum + sum(num, ...rest)
   return sum(sum, ...rest);
 };
