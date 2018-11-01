d8044c7d80447b24d9e9ffb6cc9e03ba8924fb6f
Charlie Phung <phunghiep2709@gmail.com>
2018-11-01 16:24:22 +0200

    Optimal solution

--- codewar/Number_of_trailing_zeros_of_N!.js
+++ codewar/Number_of_trailing_zeros_of_N!.js
@@ -1,37 +1,13 @@
-function isDivBy(n) {
-    return function f(m) {
-      return m % n === 0;
-    }
-  }
-  
-function countDivBy(i, j) {
-    if(j === 0) return 0;
-    var count = 0;
-    while(isDivBy(i)(j)) {
-      j /= i;
-      count++;
-    }
-    return count;
-}
+
 function zeros (n) {
-  var two = 0;
-  var five = 0;
-  var cache = {};
+ var count = 0; 
+        // Keep dividing n by powers  
+        // of 5 and update count 
+        for (let i = 5; n / i >= 1; i *= 5) 
+            count += Math.floor(n / i);
   
-  if(cache[n] !== undefined) return cache[n];
-  
-  for(let index = n; index > 0; index--) {
-    if(isDivBy(2)(index)) {
-      two += countDivBy(2, index);
-    }
-    if(isDivBy(5)(index)) {
-      five += countDivBy(5, index);
-      }
-  }
-  
-  cache[n] = two > five ? five : two;
-  
-  return cache[n];
+        return count; 
 }
 
+zeros(24999)
 
