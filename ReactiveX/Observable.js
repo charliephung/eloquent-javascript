13284fa1ed9fe8c1e3f36f8474caf0c2f1690188
charlie <charliephung@gmail.com>
2018-12-09 12:20:47 +0200

    add fromEvent to observable class

--- ReactiveX/Observable.js
+++ ReactiveX/Observable.js
@@ -1,5 +1,15 @@
 function Observable(subscribe) {
   this._subscribe = subscribe;
+
+  this.fromEvent = function(dom, eventName) {
+    return new Observable(function subscribe(observer) {
+      var handler = e => observer.onNext(e);
+      dom.addEventListener(eventName, handler);
+      return {
+        dispose: () => dom.removeEventListener(eventName, handler)
+      };
+    });
+  };
 }
 
 Observable.prototype = {
